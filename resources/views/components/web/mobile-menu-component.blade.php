71340f76b80d4a65f70240f6bbb21b52aad1e4d6
filbertmsaki <filymsaki@gmail.com>
2023-09-13 12:03:43 +0300

    changes

--- resources/views/components/web/mobile-menu-component.blade.php
+++ resources/views/components/web/mobile-menu-component.blade.php
@@ -6,80 +6,25 @@
         <div class="navigation">
             <div class="consulter-mobile-nav">
                 <ul>
+                    <li class="active">
+                        <a href="{{ route('web.index') }}">Home</a>
+                    </li>
                     <li>
-                        <a href="#">Home</a>
+                        <a href="#">About Us</a>
                         <ul>
-                            <li><a href="index.html">home 1</a></li>
-                            <li><a href="index-2.html">home 2</a></li>
-                            <li><a href="index-3.html">home 3</a></li>
-                            <li><a href="index-4.html">home 4</a></li>
-                            <li><a href="index-5.html">home 5</a></li>
+                            <li><a href="{{ route('web.about') }}">Our Company</a></li>
+                            <li><a href="{{ route('web.why_us') }}">Why Us</a></li>
+                            <li><a href="{{ route('web.our_exparience') }}">Our Exparience</a></li>
                         </ul>
                     </li>
+                    <li><a href="{{ route('web.services.index') }}">Our Services</a></li>
                     <li>
-                        <a href="services.html">Our Services</a>
-                        <ul>
-                            <li><a href="services.html">Our Services 1</a></li>
-                            <li><a href="services-2.html">Our Services 2</a></li>
-                        </ul>
+                        <a href="{{ route('web.portfolio.index') }}">Our Portfolio</a>
                     </li>
-                    <li>
-                        <a>Pages</a>
-                        <ul>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li><a href="table.html">Pricing Table</a></li>
-                            <li><a href="team.html">Team</a></li>
-                            <li><a href="team-details.html">Team Details</a></li>
-                            <li><a href="services-details.html">Services Details</a></li>
-                            <li><a href="our-project-details.html">Our Project Details</a></li>
-                            <li><a href="blog-details.html">Blog Details</a></li>
-                            <li><a href="404.html">404</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="our-project.html">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="blog.html">Blog</a>
-                        <ul>
-                            <li><a href="blog.html">Blog 1</a></li>
-                            <li><a href="blog-standard.html">Blog 2</a></li>
-                        </ul>
-                    </li>
+                    <li><a href="{{ route('web.contact') }}">Contact Us</a></li>
                 </ul>
             </div>
-            <div class="sidebar-nav__bottom mt-20">
-                <div class="sidebar-nav__bottom-contact-infos mb-20">
-                    <h6 class="color-black mb-5">Contact Info</h6>
-                    <ul>
-                        <li><a><i class="fal fa-clock"></i> Mon – Fri: 8.00 – 18.00</a></li>
-                        <li><a href="mailto:consulter@example.com"><i
-                                    class="icon-email"></i>consulter@example.com</a></li>
-                        <li>
-                            <a class="header-contact d-flex align-items-center">
-                                <div class="icon">
-                                    <i class="icon-call"></i>
-                                    <!-- <img src="assets/img/icon/phone-1.svg" alt=""> -->
-                                </div>
-                                <div class="text">
-                                    <span class="font-la mb-5 d-block fw-500">Contact For Support</span>
-                                    <h5 class="fw-500">+123 556 8824</h5>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="sidebar-nav__bottom-social">
-                    <h6 class="color-black mb-5">Follow On:</h6>
-                    <ul>
-                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                    </ul>
-                </div>
-            </div>
+            
         </div>
     </nav>
 </div>
